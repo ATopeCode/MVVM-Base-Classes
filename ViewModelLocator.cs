a1a3b1004a7e03725347c3fb6b71cb65f2104c94
Silverio Martinez Garcia <atopecode@gmail.com>
2017-07-27 19:28:38 +0200

    Create ViewModelLocator.cs

---
+++ ViewModelLocator.cs
@@ -0,0 +1,52 @@
+namespace UTILS
+{
+    public class ViewModelLocator
+    {
+        private readonly IUnityContainer _unityContainer;
+
+        //Patrón Singleton (pero se deja el constructor como público).
+        private static readonly ViewModelLocator _instance = new ViewModelLocator();
+        public static ViewModelLocator Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public ViewModelLocator()
+        {
+            _unityContainer = new UnityContainer();
+        }
+
+        public T Resolve<T>()
+        {
+            return _unityContainer.Resolve<T>();
+        }
+
+        public object Resolve(Type type)
+        {
+            return _unityContainer.Resolve(type);
+        }
+
+        public void RegisterType<T>()
+        {
+            _unityContainer.RegisterType<T>();
+        }
+
+        public void Register<T>(T instance)
+        {
+            _unityContainer.RegisterInstance<T>(instance);
+        }
+
+        public void Register<TInterface, T>() where T : TInterface
+        {
+            _unityContainer.RegisterType<TInterface, T>();
+        }
+
+        public void RegisterSingleton<TInterface, T>() where T : TInterface
+        {
+            _unityContainer.RegisterType<TInterface, T>(new ContainerControlledLifetimeManager());
+        }
+    }
+}
